aaaba1bd6b11b226de7b4778083c064295f7cd4c
Tengku Naim <naimtmas@gmail.com>
2021-05-09 15:47:32 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Database Lecturer Expert System
+
+This is a prototype of an expert system developed for WID2001 - Knowledge Representation and Reasoning.
+
+Our group decided to develop the expert system based on the course WIA2002 - Database.
---
+++ lecturer_es.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, url_for
+from datetime import datetime
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    curr_time = datetime.now()
+    hour = int(curr_time.strftime("%H"))
+
+    if hour >= 0 and hour < 12:
+        message = "Morning"
+    elif hour >= 12 and hour < 17:
+        message = "Afternoon"
+    else:
+        message = "Evening"
+
+    return render_template("home.html", message=message)
+
+
+app.run()
