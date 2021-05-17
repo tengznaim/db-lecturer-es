2a31e03ae2cb9942d7238fa882cba43590797e08
Muhammad Nurhafiz <64152220+kurkurzz@users.noreply.github.com>
2021-05-17 22:43:47 +0800

    add lecturer logic

--- backend.py
+++ backend.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import pymongo
 import os
+import json
 
 load_dotenv()
 
@@ -8,12 +9,17 @@ client = pymongo.MongoClient(os.environ.get('MONGODB_CLIENT'))
 
 db = client['dblecturer']
 
+with open('db.json') as f:
+	data = json.load(f)
+
 #collection
-chapters_collection = db['chapters']
+# chapters_collection = db['chapters']
+# # chapters_collection.insert_one(data)
 
-#insert
-# chapters_collection.insert_one({ "name": "John", "address": "Highway 37" })
+# # #insert
+# # # chapters_collection.insert_one({ "name": "John", "address": "Highway 37" })
 
-#read
-data = chapters_collection.find_one({'name':'John'})
-print(data)
+# # #read
+# data = chapters_collection.find_one({})
+# print(data['What do you want to know about Database subject?'])
+
