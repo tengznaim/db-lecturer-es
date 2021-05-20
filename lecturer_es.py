2a81d6cf1c395ca7e54de0cef6f4055e87b6bda6
Tengku Naim <naimtmas@gmail.com>
2021-05-20 23:53:09 +0800

    Added question and answer pages, refactored

--- lecturer_es.py
+++ lecturer_es.py
@@ -16,6 +16,7 @@ student_info = {
 
 curr_question = list(data.keys())[0]
 curr_response = data[curr_question]
+final_answer = ""
 
 
 @ app.route('/')
@@ -40,12 +41,12 @@ def get_info():
         student_info["Matric_Number"] = request.form["matric-number"]
         student_info["Year"] = request.form.get("year-of-study")
         student_info["Course"] = request.form.get("course")
-        return redirect("/lecturer-intro")
+        return redirect("/introduction")
 
     return render_template("information.html")
 
 
-@ app.route("/lecturer-intro", methods=["GET", "POST"])
+@ app.route("/introduction", methods=["GET", "POST"])
 def intro():
     if request.method == "POST":
         response = request.form["response-button"]
@@ -54,19 +55,57 @@ def intro():
         global curr_question
 
         next_decision = curr_response[response]
+        question = list(next_decision.keys())[0]
+        next_responses = next_decision[question]
+        curr_response = next_responses
+        curr_question = question
 
-        if isinstance(next_decision, dict):
-            question = list(next_decision.keys())[0]
-            next_responses = next_decision[question]
-            curr_response = next_responses
-            curr_question = question
-            return render_template("lecturer_intro.html", name=student_info["Name"], main_question=question, responses=next_responses)
-        else:
-            return next_decision
+        return redirect("/question")
 
     else:
         responses = list(curr_response.keys())
-        return render_template("lecturer_intro.html", name=student_info["Name"], main_question=curr_question, responses=responses)
+        return render_template("introduction.html", name=student_info["Name"], main_question=curr_question, responses=responses)
+
+
+@ app.route("/question", methods=["GET", "POST"])
+def questions():
+    if request.method == "POST":
+        response = request.form["response-button"]
+
+        global curr_response
+        global curr_question
+        global final_answer
+
+        next_decision = curr_response[response]
+
+        if isinstance(next_decision, dict):
+            question = list(next_decision.keys())[0]
+            curr_question = question
+            next_responses = next_decision[question]
+            curr_response = next_responses
+
+            return render_template("questions.html", main_question=question, responses=next_responses)
+        else:
+            curr_question = response
+            final_answer = next_decision
+            return redirect("/answer")
+
+    return render_template("questions.html", main_question=curr_question, responses=curr_response)
+
+
+@ app.route("/answer", methods=["GET", "POST"])
+def answer():
+    if request.method == "POST":
+
+        global curr_question
+        global curr_response
+
+        curr_question = list(data.keys())[0]
+        curr_response = data[curr_question]
+
+        return redirect("/introduction")
+
+    return render_template("answer.html", question=curr_question, answer=final_answer)
 
 
 app.run(debug=True)
